c62a15db3958696a27dd0f2a65fb5e1745bb2946
Peter Guan <pyg@ad.unc.edu>
2020-10-18 23:57:15 -0400

    Added utils.R, contains functions used throughout the project

---
+++ utils.R
@@ -0,0 +1,54 @@
+library(tidyverse);
+library(urltools);
+library(quanteda);
+library(scales);
+
+
+#Strips url's down to subdomain
+clean_url <- function(x, urlcol) {
+  remove_subdomain <- x %>% mutate(Match = ifelse(is.na(suffix_extract(urlcol)$subdomain), urlcol, 
+                                                  str_replace(urlcol, suffix_extract(urlcol)$subdomain, "")));
+  
+  remove_period <- remove_subdomain %>% mutate(Match = str_replace(remove_subdomain$Match, regex("^\\."), ""))
+}
+
+#Counts length of url, # of hyphens, and # of numbers
+add_parameters <- function(df, urlcol, malcol) {
+  df_enriched <- df %>% mutate(Length = str_length(urlcol)) %>% mutate(hyphen = str_count(urlcol, "-")) %>%
+    mutate(number = str_count(urlcol, regex("\\d"))) %>% mutate(Malicious = as.factor(malcol))
+  
+}
+
+#Creates character-ngrams and converts into a document-term matrix
+dfm_creator <- function(urls, ngram_length) {
+  tokens_sizen <- tokens_ngrams(tokens(urls, what = "character"), n = ngram_length, concatenator = "")
+  dfm_tokens <- dfm(tokens_sizen)
+}
+
+#Sets up tidy data in form needed for the model, also has option to trim if necessary
+model_prep <- function(urlcol, n, refOrder, trim_doc, trim_term) {
+  dfm <- dfm_creator(urlcol, n);
+  docvars(dfm, "Malicious") <- refOrder
+  dfm_trimmed <- dfm_trim(dfm, min_docfreq = trim_doc, min_termfreq = trim_term, verbose = TRUE)
+}
+
+#Uses ggplot to plot confusion matrix
+cmplot <- function(cm){
+  mytitle <- paste("Accuracy", percent_format()(cm$overall[1]),
+                   "Sensitivity", percent_format()(cm$byClass[1]),
+                   "Specificity", percent_format()(cm$byClass[2]))
+  table <- as.data.frame(cm$table)
+  table$Prediction <- as.character(table$Prediction)
+  table$Prediction <- factor(table$Prediction, levels=c("safe", "malicious"))
+  ggplot(data = table, aes(x = Reference, y = Prediction)) +
+    geom_tile(aes(fill = log(Freq)), colour = "white") +
+    scale_fill_gradient(low = "white", high = "steelblue") +
+    geom_text(aes(x = Reference, y = Prediction, label = Freq)) +
+    theme(legend.position = "none") +
+    ggtitle(mytitle)
+}
+
+write_wrapped <- function(s, file, append=FALSE){
+  s <- strwrap(s);
+  write(s,file,append=append);
+}
